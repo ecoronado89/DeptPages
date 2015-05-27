2079269ca4051f31270d7e500b65ad363b1bcc05
ecoronado89 <coreddin@gmail.com>
2015-05-27 11:30:29 -0600

    Initial checking of DeptPages

---
+++ TestNGTest/src/Util/Packages.java
@@ -0,0 +1,57 @@
+package Util;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class Packages {
+
+	public static WebDriver driver;
+	public static String baseUrl;
+	public static StringBuffer verificationErrors = new StringBuffer();
+
+	
+  @BeforeTest
+  public void beforeTest() {
+	  
+		System.setProperty("webdriver.chrome.driver", "C:/Program Files (x86)/Google/Chrome/Application/chromedriver.exe");
+		driver = new ChromeDriver(DesiredCapabilities.chrome());
+		baseUrl = "http://ecwebs01.llbean.com/llb/shop/";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get(baseUrl + "87004");
+	  
+  }
+  
+  @AfterTest
+  public void afterTest() throws Exception {
+	  
+	  driver.quit();
+	  String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	  
+  }
+
+private void fail(String verificationErrorString) {
+	// TODO Auto-generated method stub
+	
+}
+
+public static boolean isElementPresent(WebDriver driver, By by)
+{
+	try{
+		driver.findElement(by);
+		return true;
+	} 
+	catch(Exception e){
+		return false;
+	}
+	
+}
+	
+}
---
+++ TestNGTest/src/com/TestNGTest/TestNGTest.java
@@ -0,0 +1,78 @@
+package com.TestNGTest;
+
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+
+public class TestNGTest extends Util.Packages {
+	
+	Boolean Available = false;
+  
+  @Test 
+  public void main(){
+	  
+	try{  
+			  	
+		driver.findElement(By.xpath("id('swatch1_0_0')")).click();
+		driver.findElement(By.xpath("id('ShoeSize1_0_0')")).click();
+		driver.findElement(By.xpath("id('ppAddToBagBtn_0')")).click();
+	   
+		  
+		
+		Available = Util.Packages.isElementPresent(driver, By.xpath("id('ui-dialog-title-1')"));
+		
+		if(Available = true)
+		{
+		  System.out.println("ELEMENT FIND");
+
+		}
+		else{
+		  System.out.println("ELEMENT NOT FIND"); 
+		}
+				
+		} catch (Error e){
+		  verificationErrors.append(e.toString());
+		}
+  }
+
+@Test
+public void ValidName(){
+	
+	Available = Util.Packages.isElementPresent(driver, By.xpath("id('ppName')"));
+	
+	try{
+		
+		if(Available = true)
+		{
+			System.out.println("VALID NAME FIND");
+		}
+		else{
+			System.out.println("INVALID NAME");
+		}
+	}
+	catch (Error e){
+	  verificationErrors.append(e.toString());
+	}
+}
+
+@Test
+public void SizeChart(){
+	
+	Available = Util.Packages.isElementPresent(driver, By.xpath("id('attrSwatchContainer0_0_0')"));
+	
+	try{
+		
+		if(Available = true)
+		{
+			System.out.println("SIZE CHART AVAILABLE");
+		}
+		else{
+			System.out.println("SIZE CHART UNAVAILABLE");
+		}
+	}
+	catch (Error e){
+	  verificationErrors.append(e.toString());
+	}
+}
+   
+}
