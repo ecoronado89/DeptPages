ac671db609b6c7bff4ef3c7844ebac966d65e94f
ecoronado89 <coreddin@gmail.com>
2015-06-02 15:02:07 -0600

    Adding Random function to select any swatch and size

--- TestNGTest/src/com/TestNGTest/TestNGTest.java
+++ TestNGTest/src/com/TestNGTest/TestNGTest.java
@@ -1,11 +1,13 @@
 package com.TestNGTest;
 
 
+import java.util.List;
+import java.util.Random;
 import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-import org.testng.Assert;
+//import org.testng.Assert;
 
 public class TestNGTest extends Util.Packages {
 	
@@ -15,19 +17,42 @@ public class TestNGTest extends Util.Packages {
 public void ValidatePromo(){
 	  
 	try{  
-			  	
-		driver.findElement(By.xpath("id('swatch1_0_0')")).click();
-		driver.findElement(By.xpath("id('ShoeSize1_0_0')")).click();
-		driver.findElement(By.xpath("id('ppAddToBagBtn_0')")).click();	   	  
-	
+		
+		List<WebElement> Swatches = driver.findElements(By.xpath("//*[@id='colorSwatchContainer0_0_0']/ul/li"));
+		
+		
+		
+		
+			 Random rand = new Random();
+			 	int randomNum;
+			    randomNum = rand.nextInt((Swatches.size()));
+			    
+			    System.out.println(randomNum);
+			    driver.findElement(By.xpath("//*[@id='colorSwatchContainer0_0_0']/ul/li["+randomNum+"]")).click();
+			    driver.findElement(By.xpath("//*[@id='attrSwatchContainer0_0_0']/ul/li[1]")).click();
+			    driver.findElement(By.xpath("id('ppAddToBagBtn_0')")).click();
+			    AddToSB();
+
+			
 		WebElement Temp = driver.findElement(By.xpath("//*[@id='alternateProductPage']/div[4]/div[2]/div[4]/div/button[2]"));
 		Temp.click();
-
-				
+			
 		} catch (Error e){
 		  verificationErrors.append(e.toString());
 		}
   }
+ 
+ public void AddToSB(){
+	 try{
+		 Available = Util.Packages.isElementPresent(driver, By.xpath("//*[@id='alternateProductPage']/div[4]/div[2]/div[1]/div/div/ul/li[4]/div/strong"));
+		 if(Available = true)
+			 System.out.println("Promo applied");
+		 else
+			 System.out.println("Promo not applied");
+	 }catch(Error e){
+		 verificationErrors.append(e.toString()); 
+	 }
+ }
 
 @Test
 public void ValidName(){
@@ -38,12 +63,9 @@ public void ValidName(){
 	try{
 		
 		if(Available = true)
-		{
-			System.out.println("VALID NAME FIND: " + HName);
-		}
-		else{
+			System.out.println("Product Name: " + HName);
+		else
 			System.out.println("INVALID NAME");
-		}
 	}
 	catch (Error e){
 	  verificationErrors.append(e.toString());
@@ -54,16 +76,18 @@ public void ValidName(){
 @Test
 public void SizeChart(){
 	
-	Available = Util.Packages.isElementPresent(driver, By.xpath("id('attrSwatchContainer0_0_0')"));
+	@SuppressWarnings("unused")
+	Boolean X = false;
+	X = Util.Packages.isElementPresent(driver, By.xpath("//*[@id='attrStchContainer0_0_0']/a"));
 	
 	try{
 		
-		if(Available = true)
+		if(X = true)
 		{
-			System.out.println("SIZE CHART AVAILABLE");
+			System.out.println("Size Chart present");
 		}
 		else{
-			System.out.println("SIZE CHART UNAVAILABLE");
+			System.out.println("Size Chart Unavailable");
 		}
 	}
 	catch (Error e){
